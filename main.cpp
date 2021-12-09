5105b62e70fb8c9cf7de47319da136e316917ccf
MarcoUnige0226 <91851583+MarcoUnige0226@users.noreply.github.com>
2021-12-09 18:17:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main() {
     int d1=0;
     float array[d1];
-    float a=0;
+    float max=0;
     cin>>d1;
     for (int i = 0; i<d1 ; i++) {
         cin>>array[i];
     }
-cout<<Max(array,d1,a);
+cout<<Max(array,d1,max);
 
     return 0;
 }

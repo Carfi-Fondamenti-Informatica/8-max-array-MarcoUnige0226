00cd92bd4279d7bbe8a0df0bcf546189e3251377
MarcoUnige0226 <91851583+MarcoUnige0226@users.noreply.github.com>
2021-12-09 17:50:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int d1=0;
+    float array[d1];
+    float a=0;
+    cin>>d1;
+    for (int i = 0; i<d1 ; i++) {
+        cin>>array[i];
+    }
+cout<<Max(array,d1,a);
+
+    return 0;
 }

9a87c7761d149799eb984f2a86f36ffcbb650fa6
MarcoUnige0226 <91851583+MarcoUnige0226@users.noreply.github.com>
2021-12-09 22:27:03 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,6 +1,5 @@
-
 #include "lib.h"
-float Max(float array[],int d1,float max){
+float Max(const float array[],int d1,float max){
     max=array[0];
     for (int i = 0; i < d1; i++) {
         if(array[i]>max){

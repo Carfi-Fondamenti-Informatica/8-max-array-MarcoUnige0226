8e8a7f8760c6df3eadda8aebfb5b4f220896d3d1
MarcoUnige0226 <91851583+MarcoUnige0226@users.noreply.github.com>
2021-12-09 17:49:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,9 @@
-
+#include "lib.h"
+float Max(float array[],int d1,float a){
+    for (int i = 0; i < d1; i++) {
+        if(array[i]>a){
+            a=array[i];
+        }
+    }
+    return a;
+}
